34f57501eebeeabcd08a96e8150e6d8afcef5d1e
CrimsonCodex2195 <shariqali2195@gmail.com>
2021-05-12 22:26:10 -0400

    Completed Task 2

--- index.js
+++ index.js
@@ -75,8 +75,24 @@ class Airplane {
   */
   
  class Car {
-    
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
   }
+  fill(gallons) {
+    this.tank += gallons;
+  }
+  drive(distance) {
+    if(tank === 0) {
+      return `I ran out of fuel at ${this.odometer} miles!`;
+    } else {
+      this.odometer += distance;
+      this.tank -= distance / this.milesPerGallon;
+    }
+  }
+}
   
   /*
     TASK 3
